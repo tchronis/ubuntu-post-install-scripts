4f9aa7e0e69676c317a7820b1318374a1eb69f6d
Theodore Chronis <t.n.chronis@gmail.com>
2021-04-18 16:40:10 +0300

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git -y
-sudo apt-get install sudo apt install openjdk-11-jdk -y
-sudo apt install curl
+sudo apt-get install openjdk-11-jdk -y
+sudo apt install curl -y
 
