6723adcea7483942cfe46b69c4e27ae3b1270de8
Theodore Chronis <t.n.chronis@gmail.com>
2021-04-18 17:01:52 +0300

    update

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git -y
 sudo apt-get install openjdk-11-jdk -y
 sudo apt install curl -y
+sudo apt-get install htop -y
+sudo apt-fast install lm-sensors -y
 
