f281fcff72688b5a6fad28b8e6a3151319df81cc
Theodore Chronis <t.n.chronis@gmail.com>
2021-04-18 17:50:57 +0300

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 
-#Are you hearing about it for the first time? apt-get is the command you probably learnt how to use Ubuntu with. In fact, all the commands in this article use apt-get.
+#Are you hearing about it for the first time? apt-get is the command you probably learnt how to use Ubuntu with.
 #If you would like your downloads to enjoy a better speed then install apt-fast and use it in the place of apt-get.
 sudo add-apt-repository ppa:apt-fast/stable -y
+sudo apt-get install apt-fast -y
 
-sudo apt-get update && sudo apt-get upgrade -y
-
-sudo apt-get install apt-fast
+sudo apt-fast update && sudo apt-fast upgrade -y
 
 sudo apt-fast install git openjdk-11-jdk curl htop lm-sensors nodejs npm -y
 
