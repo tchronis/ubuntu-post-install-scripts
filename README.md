29f04ef5583f16f2650c69c817dfd947e69592b9
Theodore Chronis <t.n.chronis@gmail.com>
2021-04-18 20:11:42 +0300

    update

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # ubuntu-post-install-scripts
 
+sudo apt-get install git -y
+
 git clone https://github.com/tchronis/ubuntu-post-install-scripts.git
 
 cd ubuntu-post-install-scripts
-
-chmod +x *.sh
 
 ./install.sh
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-sudo apt-get install git -y
 
 #Are you hearing about it for the first time? apt-get is the command you probably learnt how to use Ubuntu with.
 #If you would like your downloads to enjoy a better speed then install apt-fast and use it in the place of apt-get.
@@ -7,15 +6,18 @@ sudo apt-get install apt-fast -y
 
 sudo apt-fast update && sudo apt-fast upgrade -y
 
-sudo apt-fast install terminator openjdk-11-jdk curl htop lm-sensors -y
+sudo apt-fast install git terminator openjdk-11-jdk curl htop lm-sensors -y
 
 sudo apt-fast install docker.io -y
 sudo systemctl enable --now docker
 sudo apt-fast  install docker-compose -y
 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.bashrc
-nvm install node
 
-#sudo npm install -g generator-jhipster
-#sudo npm install -g yo
+#must run outside this script
+#source ~/.bashrc
+
+#nvm install node
+
+#npm install -g generator-jhipster
+#npm install -g yo
