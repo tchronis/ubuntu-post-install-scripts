cf1382a6cc1c094778085a3f61c1a139671f88c9
Theodore Chronis <t.n.chronis@gmail.com>
2021-06-26 07:30:08 +0200

    add rustc and vscode extensions

--- install.sh
+++ install.sh
@@ -37,7 +37,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install node
+nvm install node #Run this again to update node and npm
 
 npm install -g generator-jhipster
 npm install -g yo
+
+sudo apt-fast install rustc -y
