17d89af878bf99ab349b309ebf0c2ab4160f4041
Theodore Chronis <t.n.chronis@gmail.com>
2021-10-23 18:55:21 +0200

    install lts npm

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install node #Run this again to update node and npm
+nvm install --lts #Install the latest LTS release of Node.js
 
 npm install -g generator-jhipster
 npm install -g yo
